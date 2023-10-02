ad8173bb0724bde65c12d555994410d573e1ef68
narendra015 <narendrapamungkas07@gmail.com>
2023-10-02 11:50:25 +0700

    upload

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,13 +1,60 @@
+const dataSepatu = require('./sepatu.json');
+
+const fs = require("fs");
+
+const path = require("path");
+
+const filePath = path.join(__dirname,'sepatu.json');
 function getSepatu(req, res, next){
-    res.json({
-        message:"ini get list product"
-    })
+    let sepatuFs = [];
+    fs.readFile(filePath,"utf8",(err,jsonString)=> {
+    if (err){
+        console.log("Error reading file from disk:",err);
+        res.status(500).json({error: 'Error reading JSON file'});
+        return;
+    }
+    try
+    {
+        sepatuFs = JSON.parse(jsonString);
+        res.json({sepatuFs}); 
+    }
+    catch(err)
+    {
+        console.log("Error parsing JSON string :", err);
+    }
+    });
 }
 
 function addSepatu(req, res, next){
-    res.json({
-        message:"ini Add product"
-    })
+    const newData =req.body;
+
+    fs.readFile(filePath,"utf8",(err,jsonString)=> {
+        if (err){
+            console.log("Error reading file from disk:",err);
+            res.status(500).json({error: 'Error reading JSON file'});
+            return;
+        }
+        try
+        {
+            const existingData = JSON.parse(jsonString);
+            existingData.push(newData);
+            const updateData = JSON.stringify(existingData,null,2);
+            fs.writeFile(filePath,updateData,(writeErr) => {
+                if(writeErr){
+                    console.log('Error writing JSON file :',writeErr);
+                    res.status(500).json({ error: 'Error writing JSON file'});
+                }
+                else{
+                    res.json({message : 'Data added successfully'});
+                }
+            });
+        }
+        catch(parseError)
+        {
+            console.log("Error parsing JSON string :", parseError);
+            res.status(500).json({ error: 'Error parsing JSON file'});
+        }
+        });
 }
 
 module.exports={
